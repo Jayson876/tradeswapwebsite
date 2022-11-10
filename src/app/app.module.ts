957907548844e7141498ae32422ced78203d08c1
Jason <dljvaz@gmail.com>
2022-11-10 09:56:59 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './public/pages/home/home.component';
+import { ContactusComponent } from './public/pages/contactus/contactus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactusComponent
   ],
   imports: [
     BrowserModule,
