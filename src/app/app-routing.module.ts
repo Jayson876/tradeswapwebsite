43d4d79a68183f1b76afe2bfc66e796eeb762a84
Warren Feurtado <wolfefeurtado@gmail.com>
2022-11-11 13:26:12 -0500

    Worker list component under construnction

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './public/pages/home/home.component';
+import { WorkersListComponent } from './workers-list/workers-list.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'workers', component: WorkersListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
